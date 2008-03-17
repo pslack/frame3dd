2b9bcb41ae1162e5de81d139789a0c12d4c2bec3
jdpipe <jdpipe@0071cf9a-5322-479b-8c99-447f13b1adbf>
2008-03-17 01:41:20 +0000

    Merging Henri's changes to rename project to 'FRAME3DD'.
    git-svn-id: https://svn.code.sf.net/p/frame3dd/code/trunk@58 0071cf9a-5322-479b-8c99-447f13b1adbf

--- src/eig.h
+++ src/eig.h
@@ -1,15 +1,15 @@
-/*	FRAME: Static and dynamic structural analysis of 2D & 3D frames and trusses
-	Copyright (C) 1992-2007  Henri P. Gavin
+/*	FRAME3DD: Static and dynamic structural analysis of 2D & 3D frames and trusses
+	Copyright (C) 1992-2008  Henri P. Gavin
 
-	This program is free software; you may redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- src/lu_dcmp.h
+++ src/lu_dcmp.h
@@ -1,3 +1,20 @@
+/*	FRAME3DD: Static and dynamic structural analysis of 2D & 3D frames and trusses
+	Copyright (C) 1992-2008  Henri P. Gavin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef FRAME_LU_DCMP_H
 #define FRAME_LU_DCMP_H
 
