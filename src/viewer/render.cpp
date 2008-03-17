31750c6ee8d954b7b54c7d3bcb263e70b6842bed
jdpipe <jdpipe@0071cf9a-5322-479b-8c99-447f13b1adbf>
2008-03-17 04:33:39 +0000

    Couple more GPL headers added.
    git-svn-id: https://svn.code.sf.net/p/frame3dd/code/trunk@63 0071cf9a-5322-479b-8c99-447f13b1adbf

--- src/viewer/render.cpp
+++ src/viewer/render.cpp
@@ -1,20 +1,21 @@
-/*	VIEW3D
-	Copyright (C) 2008 John Pye
+/*	FRAME3DD: Static and dynamic structural analysis of 2D & 3D frames and trusses
+	Copyright (C) 2007-2008 John Pye
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2, or (at your option)
-	any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place - Suite 330,
-	Boston, MA 02111-1307, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*//* @FILE
+	Some rendering routines for use with the Open Inventor API that allow
+	high-level objects to be added to the scenegraph easily.
 */
 #include "render.h"
 
