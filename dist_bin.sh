990f679b04f5831d0fcc5e1ab492a1075c97377d
hpgavin <hpgavin@0071cf9a-5322-479b-8c99-447f13b1adbf>
2009-10-20 22:17:52 +0000

    assemble binary and source distribution .zip files
    git-svn-id: https://svn.code.sf.net/p/frame3dd/code/trunk@288 0071cf9a-5322-479b-8c99-447f13b1adbf

---
+++ dist_bin.sh
@@ -0,0 +1,70 @@
+# !bin/bash
+#
+# dist_zip.sh
+# assemble .ZIP files for Frame3dd binary distribution release
+
+# clean out prior distribution files
+echo "cleaning out prior distribution files ... "
+rm -rf dist/Frame3DD
+
+# make directory structure for distribution .ZIP files in the ./dist directory
+echo "creating directory structure for new distribution ... "
+mkdir dist/Frame3DD
+mkdir dist/Frame3DD/doc
+mkdir dist/Frame3DD/doc/img
+mkdir dist/Frame3DD/doc/Matlab
+mkdir dist/Frame3DD/examples
+
+# copy documentation
+echo "copying documentation ... "
+cp --preserve=mode,timestamps doc/*.html            dist/Frame3DD/doc/.
+cp --preserve=mode,timestamps doc/template.3dd      dist/Frame3DD/doc/.
+cp --preserve=mode,timestamps doc/img/*.png         dist/Frame3DD/doc/img/.
+cp --preserve=mode,timestamps doc/img/*.jpg         dist/Frame3DD/doc/img/.
+cp --preserve=mode,timestamps doc/img/*.gif         dist/Frame3DD/doc/img/.
+cp --preserve=mode,timestamps doc/Matlab/*          dist/Frame3DD/doc/Matlab/.
+cp --preserve=mode,timestamps doc/LinuxTerminal.pdf dist/Frame3DD/doc/.	
+
+# copy ChangeLog, license, and README files
+cp --preserve=mode,timestamps ChangeLog.txt         dist/Frame3DD/.
+cp --preserve=mode,timestamps LICENSE.txt           dist/Frame3DD/.
+cp --preserve=mode,timestamps README.txt            dist/Frame3DD/.
+cp --preserve=mode,timestamps README-win32.txt      dist/Frame3DD/.
+
+# copy example files
+echo "copying example files ... "
+cp --preserve=mode,timestamps examples/ex*.3dd      dist/Frame3DD/examples/.
+cp --preserve=mode,timestamps examples/ex*.out      dist/Frame3DD/examples/.
+
+# copy executables
+echo "copying executables ... "
+cp --preserve=mode,timestamps build/frame3dd.exe    dist/Frame3DD/.
+cp --preserve=mode,timestamps build/frame3dd        dist/Frame3DD/.
+cp --preserve=mode,timestamps build/frame3ddosx     dist/Frame3DD/.
+
+# assemble the .zip file
+echo "assembling .zip file ... "
+cd dist
+zip -r Frame3DD_$(date +%Y%m%d).zip Frame3DD/*
+
+# delete non-os executables from each .zip distribution
+cp Frame3DD_$(date +%Y%m%d).zip Frame3DD_$(date +%Y%m%d)_linux.zip
+cp Frame3DD_$(date +%Y%m%d).zip Frame3DD_$(date +%Y%m%d)_osx.zip
+cp Frame3DD_$(date +%Y%m%d).zip Frame3DD_$(date +%Y%m%d)_win32.zip
+
+zip -d Frame3DD_$(date +%Y%m%d)_linux.zip Frame3DD/frame3dd.exe
+zip -d Frame3DD_$(date +%Y%m%d)_linux.zip Frame3DD/frame3ddosx
+
+zip -d Frame3DD_$(date +%Y%m%d)_osx.zip Frame3DD/frame3dd.exe
+zip -d Frame3DD_$(date +%Y%m%d)_osx.zip Frame3DD/frame3dd
+
+zip -d Frame3DD_$(date +%Y%m%d)_win32.zip Frame3DD/frame3ddosx
+zip -d Frame3DD_$(date +%Y%m%d)_win32.zip Frame3DD/frame3dd
+
+rm Frame3DD_$(date +%Y%m%d).zip
+
+echo "Frame3DD .zip archive complete"
+
+# ----------------------------------------------------------- dist_bin.sh
+# Henri P. Gavin
+# October 20 2009
---
+++ dist_src.sh
@@ -0,0 +1,38 @@
+# !bin/bash
+#
+# dist_src.sh
+# assemble .ZIP files for Frame3dd source code distribution release
+
+# clean out prior distribution files
+echo "cleaning out prior distribution files ... "
+rm -rf dist/Frame3DD
+
+# make directory structure for distribution .ZIP files in the ./dist directory
+echo "creating directory structure for new distribution ... "
+mkdir dist/Frame3DD
+mkdir dist/Frame3DD/src
+mkdir dist/Frame3DD/src/microstran
+
+# copy documentation
+echo "copying documentation ... "
+cp --preserve=mode,timestamps src/*.c               dist/Frame3DD/src/.
+cp --preserve=mode,timestamps src/*.h               dist/Frame3DD/src/.
+cp --preserve=mode,timestamps src/microstran/vec3.* dist/Frame3DD/src/microstran/.
+cp --preserve=mode,timestamps src/microstran/config.h dist/Frame3DD/src/microstran/.
+
+# copy ChangeLog, license, and README files
+cp --preserve=mode,timestamps ChangeLog.txt         dist/Frame3DD/.
+cp --preserve=mode,timestamps LICENSE.txt           dist/Frame3DD/.
+cp --preserve=mode,timestamps README.txt            dist/Frame3DD/.
+cp --preserve=mode,timestamps README-win32.txt      dist/Frame3DD/.
+
+# assemble the .zip file
+echo "assembling .zip file ... "
+cd dist
+zip -r Frame3DD_$(date +%Y%m%d)_src.zip Frame3DD/*
+
+echo "Frame3DD .zip archive complete"
+
+# ----------------------------------------------------------- dist_src.sh
+# Henri P. Gavin
+# October 20 2009
