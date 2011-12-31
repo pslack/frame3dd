2d38693a2faff94adb5701f3c237d0a7d00c3234
hpgavin <hpgavin@0071cf9a-5322-479b-8c99-447f13b1adbf>
2011-12-31 00:11:50 +0000

    add GPL lines to matrix.c
    git-svn-id: https://svn.code.sf.net/p/frame3dd/code/trunk@460 0071cf9a-5322-479b-8c99-447f13b1adbf

--- src/matrix.c
+++ src/matrix.c
@@ -1,4 +1,30 @@
 /*
+ This file is part of FRAME3DD:
+ Static and dynamic structural analysis of 2D and 3D frames and trusses with
+ elastic and geometric stiffness.
+ ---------------------------------------------------------------------------
+ http://frame3dd.sourceforge.net/
+ ---------------------------------------------------------------------------
+ Copyright (C) 1992-2009  Henri P. Gavin
+ 
+ FRAME3DD is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ FRAME3DD is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FRAME3DD.  If not, see <http://www.gnu.org/licenses/>.
+*//**
+        @file
+        matrix decomposition, back-substitution, and multiplication
+*/
+
+/*
  * ==========================================================================
  *
  *       Filename:  matrix.c
